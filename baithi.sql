a3748238574b08586782f85edac20f52feaad6da
Cường Lê <161845711+cuongle4399@users.noreply.github.com>
2024-06-21 12:34:51 +0700

    Add files via upload

---
+++ baithi.sql
@@ -0,0 +1,7 @@
+﻿select a.manganh, count(a.manganh) as N'Số lượng sinh viên của ngành đã tuyển sinh',
+case 
+when count(a.manganh) > 300 Then N'Ngành Thế mạnh'
+else N'Ngành Tiềm năng'
+end as N'trạng thai' from sinhvien as a join nganh as b
+on a.manganh=b.manganh
+group by a.manganh
